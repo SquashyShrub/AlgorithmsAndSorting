93bbc8721557259abc24dda5dd0a18bcbb9237a4
joshu <joshuacrandall30@outlook.com>
2024-11-07 10:49:00 -0600

    Insertion sort summary and algorithm completed

--- Program.cs
+++ Program.cs
@@ -58,9 +58,53 @@
             }
         }
 
+        /// <summary>
+        /// Insertion sort builds the sorted array one item at a time by inserting each element into its proper position
+        /// in an already sorted portion of the array
+        /// 
+        /// STEPS (for ascending order)
+        /// 1. Select one element from the left in the collection    (12) (34) 56 78 6
+        /// 2. Insert this element in proper location after comparison    (12) (34) 56 78 6 ⇒ minPos = 0
+        /// 3. After positions are interchanged, ensure all elements to the left are sorted    [(12) 34 78 56 (6)] ⇒ minPos = 4 ⇒ [6 34 56 23 12]
+        /// 4. Repeat steps 1 - 3 until sorted
+        /// 
+        /// TIME COMPLEXITY
+        /// * BEST: O(n)  ;  Worst: O(n^2)  :  Average O(n^2);
+        /// 
+        /// SPACE COMPLEXITY
+        /// * O(1)
+        /// 
+        /// BEST USE CASES
+        /// * Small data sets, simple applications, applications where stability is important
+        /// 
+        /// PROS
+        /// * Very efficient for small or nearly sorted data sets
+        /// * Stable - preserves relative order of equal elements
+        /// * Adaptive - can be optimized to stop early if the list is already sorted
+        /// 
+        /// CONS
+        /// * Slow on large data sets
+        /// * Slower than other algorithms such as Quick Sort or Merge Sort
+        /// 
+        /// </summary>
+        /// <param name="arr"></param>
         static void InsertionSort(int[] arr)
         {
+            int temp; 
+            int position = 0;
 
+            for (int i = 1; i < arr.Length; i++)
+            {
+                temp = arr[i];
+                position = i;
+
+                while (position > 0 && arr[position - 1] > temp)
+                {
+                    arr[position] = arr[position - 1];
+                    position--;
+                }
+                arr[position] = temp;
+            }
         }
 
         static void QuickSort(int[] arr)
