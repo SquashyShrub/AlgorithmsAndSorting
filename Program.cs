6d09f07e0ea9d70343d13876d583e0e0a51b81cc
SquashyShrub <joshuacrandall2003@gmail.com>
2026-04-23 12:52:43 -0700

    Finalized SelectionSort Class and removed its implementation in main.

--- Program.cs
+++ Program.cs
@@ -11,12 +11,12 @@ namespace SortingPractice
             int[] ints3 = { 100, 60, 40, 30, 2 };
             int[] ints4 = { 78, 34, 10, 67, 54 };
 
-            InsertionSort insertionSorter = new InsertionSort();
+            SelectionSort selectionSorter = new SelectionSort();
             foreach(int num in ints1) {
                 System.Console.Write(num + " ");
             }
             
-            insertionSorter.sort(ints1);
+            selectionSorter.sort(ints1);
 
             System.Console.WriteLine("Sorted array:");
             foreach (int num in ints1)
@@ -24,62 +24,6 @@ namespace SortingPractice
                 System.Console.Write(num + " ");
             }
             System.Console.WriteLine();
-        }
-
-
-        /// <summary>
-        /// Selection sort works by repeatedly finding the minimum element from the unsorted portion of the array
-        /// and swapping it with the first unsorted element
-        /// 
-        /// STEPS (for ascending order)
-        /// 1. Compare one element with the rest of the array    (12) (34) 56 78 6
-        /// 2. IAfter comparison, determine if a new minimum occurs   (12) (34) 56 78 6 ⇒ minPos = 0
-        /// 3. Continue comparing the next set of elements    [(12) 34 78 56 (6)] ⇒ minPos = 4 ⇒ [6 12 34 56 23]
-        /// 4. Repeat steps 1 - 3 until sorted
-        /// 
-        /// TIME COMPLEXITY
-        /// * BEST: O(n^2)  ;  Worst: O(n^2)  :  Average O(n^2);
-        /// 
-        /// SPACE COMPLEXITY
-        /// * O(1)
-        /// 
-        /// BEST USE CASES
-        /// * Small data sets, simple applications, memory usage is a concern
-        /// 
-        /// PROS
-        /// * Simple and easy to implement
-        /// * In-place sorting with O(1) space complexity
-        /// * Does not require additional memory
-        /// 
-        /// CONS
-        /// * Slow and inefficient on large data sets
-        /// * Not stable - can change the order of equal elements
-        /// 
-        /// </summary>
-        /// <param name="arr"></param>
-        static void SelectionSort(int[] arr)
-        {
-            int minPos = 0; //track the position(index) of the minimum element
-
-            for (int i = 0; i < arr.Length - 1; i++)
-            {
-                minPos = i;
-                for (int j = i + 1; j < arr.Length; j++)
-                {
-                    if (arr[j] < arr[minPos])
-                        minPos = j;
-                }
-                if (minPos != i)
-                {
-                    Swap(arr[i], arr[i+1]);
-                }
-            }
-            void Swap(int num1, int num2)
-            {
-                int temp = num1;
-                num1 = num2;
-                num2 = temp;
-            }
         }
 
 
@@ -143,7 +87,7 @@ namespace SortingPractice
                     i++;
                 }
                 //If we find a higher value, keep moving left (to find a partition)
-                while (i <= j && arr[j] > pivot) ;
+                while (i <= j && arr[j] > pivot)
                 {
                     j--;
                 }
--- SelectionSort.cs
+++ SelectionSort.cs
@@ -2,6 +2,57 @@ namespace SortingPractice
 {
     public class SelectionSort
     {
-        
+        public void sort(int[] arr)
+        {
+            /// <summary>
+            /// Selection sort works by repeatedly finding the minimum element from the unsorted portion of the array
+            /// and swapping it with the first unsorted element
+            /// 
+            /// STEPS (for ascending order)
+            /// 1. Compare one element with the rest of the array    (12) (34) 56 78 6
+            /// 2. IAfter comparison, determine if a new minimum occurs   (12) (34) 56 78 6 ⇒ minPos = 0
+            /// 3. Continue comparing the next set of elements    [(12) 34 78 56 (6)] ⇒ minPos = 4 ⇒ [6 12 34 56 23]
+            /// 4. Repeat steps 1 - 3 until sorted
+            /// 
+            /// TIME COMPLEXITY
+            /// * BEST: O(n^2)  ;  Worst: O(n^2)  :  Average O(n^2);
+            /// 
+            /// SPACE COMPLEXITY
+            /// * O(1)
+            /// 
+            /// BEST USE CASES
+            /// * Small data sets, simple applications, memory usage is a concern
+            /// 
+            /// PROS
+            /// * Simple and easy to implement
+            /// * In-place sorting with O(1) space complexity
+            /// * Does not require additional memory
+            /// 
+            /// CONS
+            /// * Slow and inefficient on large data sets
+            /// * Not stable - can change the order of equal elements
+            /// 
+            /// </summary>
+            /// <param name="arr"></param>
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int minPos = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[minPos])
+                        minPos = j;
+                }
+                if (minPos != i)
+                {
+                    swap(ref arr[i], ref arr[minPos]);
+                }
+            }
+        }
+        void swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
     }
 }
